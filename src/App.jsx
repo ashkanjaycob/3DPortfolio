d9363a5885bcea4e490b9a78747ed06b4a6051ff
Ashkan Yaghobi <ashkan.yaghobii@gmail.com>
2024-02-20 03:14:24 +0330

    Initilize Utils & app & Define Navbar

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+/* eslint-disable no-unused-vars */
+import { BrowserRouter } from "react-router-dom";
+import { About , Contact , Experience , Feedbacks , Hero , Navbar , Tech , Works} from "./components"
+
+function App() {
+
+
+  return (
+    <>
+    <BrowserRouter>
+      <div className="relative z-0 bg-primary">
+
+        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+          <Navbar />
+          <Hero />
+          <About />
+          <Experience />
+          <Tech/>
+          <Works/>
+          <Feedbacks />
+          <div className="relative z-0">
+              <Contact />
+          </div>
+        </div>
+
+      </div>
+    </BrowserRouter>
+    </>
+  )
+}
+
+export default App
