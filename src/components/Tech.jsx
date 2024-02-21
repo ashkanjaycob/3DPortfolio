eef1ccf0bbb6a0a74811722a391fe44165518cbe
Ashkan Yaghobi <ashkan.yaghobii@gmail.com>
2024-02-21 16:56:35 +0330

    Tech Section added , Float-Move-Bugs

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import { BallCanvas } from "./canvas";
+import { SectionWrapper } from "../HOC";
+import { technologies } from "../constants";
 
 const Tech = () => {
   return (
-    <div>Tech</div>
-  )
-}
+    <>
+      <div className="flex flex-row flex-wrap justify-center gap-10">
+        {technologies.map((tech) => (
+          <div className="w-28 h-28" key={tech.name}>
+            <BallCanvas icon={tech.icon} />
+          </div>
+        ))}
+      </div>
+    </>
+  );
+};
 
-export default Tech
+// eslint-disable-next-line react-refresh/only-export-components
+export default SectionWrapper(Tech, " ");
--- src/components/canvas/Ball.jsx
+++ src/components/canvas/Ball.jsx
@@ -1,9 +1,63 @@
-import React from 'react'
+/* eslint-disable react/no-unknown-property */
+import { Suspense } from "react";
+import { Canvas } from "@react-three/fiber";
+import { Decal, OrbitControls, Preload, useTexture } from "@react-three/drei";
+import CanvasLoader from "../Loader";
+import {Float} from "@react-three/drei"
 
-const Ball = () => {
+
+// eslint-disable-next-line react/prop-types
+const BallCanvas = ({ icon }) => {
   return (
-    <div>Ball</div>
-  )
-}
+    <Canvas
+      frameloop="demand"
+      gl={{ preserveDrawingBuffer: true }}
+    >
+      <Suspense fallback={<CanvasLoader />}>
+        <OrbitControls
+          enableZoom={false}
+        />
+        <Ball imgUrl={icon} />
+      </Suspense>
+      <Preload all />
+    </Canvas>
+  );
+};
 
-export default Ball
+
+// eslint-disable-next-line react/prop-types
+const Ball = ({imgUrl}) => {
+  // eslint-disable-next-line react/prop-types
+  const [decal] = useTexture([imgUrl]);
+
+  return (
+    <>
+      <Float 
+        speed={1.5} // Animation speed, defaults to 1
+        rotationIntensity={1.75} // XYZ rotation intensity, defaults to 1
+        floatIntensity={3} // Up/down float intensity, works like a multiplier with floatingRange,defaults to 1
+        >
+        <ambientLight intensity={0.25}/>
+        <directionalLight position={[0, 0, 0.5]}/> 
+        <mesh castShadow receiveShadow scale={2.75}>
+          <icosahedronGeometry args={[1, 1]} />
+          <meshStandardMaterial 
+            color="#B7C9F2"
+            polygonOffset
+            polygonOffsetFactor={-5}
+            flatShading
+          />
+          <Decal
+            position={[0, 0, 1]}
+            flatShading
+            rotation={[2 * Math.PI, 0, 6.25]}
+            map={decal}
+          />
+        </mesh>
+      </Float>
+    </>
+  );
+};
+
+
+export default BallCanvas;
